b6f7145d2dce36fbea9485d86688050875953d5f
= <xsmallo@gmail.com>
2025-04-08 19:00:58 +0200

    Fix: missing base config values

--- lua/spelunk/config.lua
+++ lua/spelunk/config.lua
@@ -1,41 +1,43 @@
 local M = {}
 
-local skipkey = 'NONE'
+local skipkey = "NONE"
 
 local default_config = {
 	base_mappings = {
-		toggle = '<leader>bt',
-		toggle_file = '<leader>bf',
-		add = '<leader>ba',
-		add_file = '<leader>bfa',
-		next_bookmark = '<leader>bn',
-		prev_bookmark = '<leader>bp',
-		search_bookmarks = '<leader>bg',
-		search_current_bookmarks = '<leader>bc',
-		search_stacks = '<leader>bs',
+		toggle = "<leader>bt",
+		add = "<leader>ba",
+		next_bookmark = "<leader>bn",
+		prev_bookmark = "<leader>bp",
+		search_bookmarks = "<leader>bg",
+		search_current_bookmarks = "<leader>bc",
+		search_stacks = "<leader>bs",
+		toggle_file = "<leader>bf",
+		add_file = "<leader>bfa",
+		next_file = "<leader>bfn",
+		previous_file = "<leader>bfp",
 	},
 	window_mappings = {
-		cursor_down = 'j',
-		cursor_up = 'k',
-		bookmark_down = '<C-j>',
-		bookmark_up = '<C-k>',
-		goto_bookmark = '<CR>',
-		goto_bookmark_hsplit = 'x',
-		goto_bookmark_vsplit = 'v',
-		delete_bookmark = 'd',
-		next_stack = '<Tab>',
-		previous_stack = '<S-Tab>',
-		new_stack = 'n',
-		delete_stack = 'D',
-		edit_stack = 'E',
-		close = 'q',
-		help = 'h',
+		cursor_down = "j",
+		cursor_up = "k",
+		bookmark_down = "<C-j>",
+		bookmark_up = "<C-k>",
+		goto_bookmark = "<CR>",
+		goto_bookmark_hsplit = "x",
+		goto_bookmark_vsplit = "v",
+		delete_bookmark = "d",
+		next_stack = "<Tab>",
+		previous_stack = "<S-Tab>",
+		new_stack = "n",
+		delete_stack = "D",
+		edit_stack = "E",
+		close = "q",
+		help = "h",
 	},
 	enable_persist = false,
-	statusline_prefix = '🔖',
-	orientation = 'vertical',
+	statusline_prefix = "🔖",
+	orientation = "vertical",
 	enable_status_col_display = false,
-	cursor_character = '>',
+	cursor_character = ">",
 }
 
 ---@param target table
@@ -74,17 +76,16 @@ M.set_keymap = function(key, cmd, description)
 		if val == skipkey then
 			return
 		end
-		vim.keymap.set('n', val, cmd,
-			{ desc = description, noremap = true, silent = true })
+		vim.keymap.set("n", val, cmd, { desc = description, noremap = true, silent = true })
 	end
-	if type(key) == 'string' then
+	if type(key) == "string" then
 		apply(key)
-	elseif type(key) == 'table' then
+	elseif type(key) == "table" then
 		for _, k in pairs(key) do
 			apply(k)
 		end
 	else
-		error('[spelunk.nvim] config.set_keymap passed unsupported type: ' .. type(key))
+		error("[spelunk.nvim] config.set_keymap passed unsupported type: " .. type(key))
 	end
 end
 
@@ -97,23 +98,22 @@ M.set_buf_keymap = function(bufnr)
 		if val == skipkey then
 			return
 		end
-		vim.api.nvim_buf_set_keymap(bufnr, 'n', val, f, { noremap = true, silent = true, desc = desc })
+		vim.api.nvim_buf_set_keymap(bufnr, "n", val, f, { noremap = true, silent = true, desc = desc })
 	end
 	---@param key string | string[]
 	---@param func string
 	---@param description string
 	return function(key, func, description)
-		if type(key) == 'string' then
+		if type(key) == "string" then
 			apply(key, func, description)
-		elseif type(key) == 'table' then
+		elseif type(key) == "table" then
 			for _, k in pairs(key) do
 				apply(k, func, description)
 			end
 		else
-			error('[spelunk.nvim] config.set_buf_keymap passed unsupported type: ' .. type(key))
+			error("[spelunk.nvim] config.set_buf_keymap passed unsupported type: " .. type(key))
 		end
 	end
 end
-
 
 return M
